ca4f94ec4d6704c2a2e289baf9128364651756ba
bhufford <bhufford@q-centrix.com>
2022-08-16 20:10:23 +0000

    Add many to many associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_133731) do
+ActiveRecord::Schema.define(version: 2022_08_16_200307) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
